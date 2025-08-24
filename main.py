4eced6895048616ef706f8aff978d4cfc1d7d08f
Théophile Lahaussois <theophile.lahaussois@student-cs.fr>
2025-08-24 19:47:03 +0200

    Creation of main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import argparse
+from MLP_quadratique import main_inference
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Predict 2D quadratic curve images from parameters a, b, c"
+    )
+    parser.add_argument(
+        "--coeffs", type=float, nargs=3, required=True,
+        help="Three coefficients a b c for the quadratic curve"
+    )
+    parser.add_argument(
+        "--out", type=str, default="predictions",
+        help="Output folder for predicted images"
+    )
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    a, b, c = args.coeffs
+    main_inference(a, b, c, output_folder=args.out)
