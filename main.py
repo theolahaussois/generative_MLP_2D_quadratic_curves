a2dcbd43f2b235efc1c2b0d671228ed9ab2d7959
Théophile Lahaussois <theophile.lahaussois@student-cs.fr>
2025-08-25 09:11:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,14 @@ def parse_args():
         "--out", type=str, default="predictions",
         help="Output folder for predicted images"
     )
+
+    parser.add_argument(
+    "--checkpoint", type=str, required=True,
+    help="Path to model checkpoint (.pth file)"
+    )
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_args()
     a, b, c = args.coeffs
-    main_inference(a, b, c, output_folder=args.out)
+    main_inference(a, b, c, checkpoint=args.checkpoint, output_folder=args.out)
